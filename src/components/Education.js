a9fbe44c3f52f371c68082bbd6ef70ac35b33126
cam-cc <caceron@protonmail.com>
2023-05-04 11:44:46 -0400

    minor changes (typos, bugs, etc)

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,13 +7,13 @@ const Education = () => {
       <h2>Education</h2>
       <div className="education__container">
         <div className="education__item">
-          <h3 className="education__degree">Advanced Diploma Compuer Programming & Analysis</h3>
+          <h3 className="education__degree">Advanced Diploma Computer Programming & Analysis</h3>
           <p className="education__university">Seneca College</p>
           <p className="education__date">Graduated June 2021</p>
         </div>
         <div className="education__item">
-          <h3 className="education__degree">Certificate Data science & Machine Learning</h3>
-          <p className="education__university"> Massachussets Institute of Technology - IDSS </p>
+          <h3 className="education__degree">Certificate Data Science & Machine Learning</h3>
+          <p className="education__university"> Massachussetts Institute of Technology - IDSS </p>
           <p className="education__date">Graduated May 2023</p>
         </div>
       </div>
